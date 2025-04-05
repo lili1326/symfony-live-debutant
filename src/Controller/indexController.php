35cb93bcd8a2ee46f651318e5e2d9d3dd505fb7e
aurelie <lieange1326@outlook.com>
2025-04-05 09:46:06 +0200

    feat:add route-controller

---
+++ src/Controller/indexController.php
@@ -0,0 +1,18 @@
+<?php
+// src/Controller/IndexController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class indexController extends AbstractController
+{
+    #[Route('/')]
+    public function index(): Response
+    {      
+       // return new Response('Hello world');
+       return $this->render('page/index.html.twig');
+    }
+
+}
